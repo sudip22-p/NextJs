5d47a3043847549087893c47b6492b656f4667bc
sudip22-p <paudelsudip134@gmail.com>
2024-10-01 11:05:17 +0545

    images in next js

--- first/app/page.js
+++ first/app/page.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
     <div>
+      <Image
+        src="https://cdn.imagecomics.com/assets/i/series/1051676/1024IM294_83163661841cffc2fecf05c4d36b5571.jpg"//external image ,need to add domain in next.config.js
+        alt="Avatar"
+        width={500}
+        height={500}
+      />
     Home page here....
     </div>
   )
