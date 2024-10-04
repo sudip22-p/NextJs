30c1c94a64f64ebb6b35ad4e9f325ed8e959e24c
sudip22-p <paudelsudip134@gmail.com>
2024-10-04 13:07:45 +0545

    server actions

--- first/app/contact/page.js
+++ first/app/contact/page.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import Script from 'next/script'
+import fs from 'fs/promises'
 const contact = () => {
+  const contactSendAction = async(e)=>{
+    "use server"
+    let name = e.get('name');
+    let email = e.get('email');
+    let message = e.get('message');
+    let a=fs.writeFile('contact.txt', `name: ${name}, email: ${email}, message: ${message}`);
+    if(a){
+      console.log('message sent')
+    }
+    else{
+      console.log('message not sent')
+    }
+  }
   return (
     <div>
       <Script>
@@ -10,7 +24,15 @@ const contact = () => {
           `
         }
       </Script>
-      hello from contact page
+      <h1 className='text-white font-bold w-[90vw] text-center mt-7'>Contact Page</h1>
+        {/* make contact form */}
+        <form action={contactSendAction} className='flex justify-center items-center gap-7 w-[90vw] h-[90vh] flex-col'>
+          <input type="text" name="name" placeholder="name" className='text-black'/>
+          <input type="email" name="email" placeholder="email" className='text-black'/>
+          <textarea name="message" placeholder="message" className='text-black'></textarea>
+          <button type="submit" className='border-2 border-white p-2 cursor-pointer'>Send</button>
+        </form>
+
     </div>
   )
 }
