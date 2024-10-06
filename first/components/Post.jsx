8ae394f776524d8884c159105ad6216b2547a044
sudip22-p <paudelsudip134@gmail.com>
2024-10-06 23:08:43 +0545

    redirection using Link

--- first/components/Post.jsx
+++ first/components/Post.jsx
@@ -1,20 +1,22 @@
 "use client";
 import React from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 const Post = ({ postNumber, callFrom }) => {
-  const onClickHandler =
-    callFrom === "multipleBlogPage" ? () => handleClick(postNumber) : null;
-  const router = useRouter();
-  const handleClick = (postNumber) => {
-    router.push(`/post/${postNumber}`);
-  };
+    if (callFrom === "multipleBlogPage") {
+        return (
+            <Link href={`/post/${postNumber}`}> 
+            <div
+              className="border border-gray-300 p-4 rounded-lg shadow-md cursor-pointer w-[25vw] bg-green-900 h-[20vh]"
+            >
+              <h2 className="text-xl font-bold mb-2">Post {postNumber}</h2>
+              <p>This is the content of post {postNumber}.</p>
+            </div>
+            </Link>
+          );
+    }
   return (
     <div
-      key={postNumber}
-      className={`border border-gray-300 p-4 rounded-lg shadow-md cursor-pointer ${
-        callFrom === "multipleBlogPage" ? "w-[25vw] bg-green-900" : "w-[90vw] bg-yellow-900"
-      } h-[20vh]`}
-      onClick={onClickHandler}
+      className="border border-gray-300 p-4 rounded-lg shadow-md cursor-pointer w-[90vw] bg-yellow-900 h-[20vh]"
     >
       <h2 className="text-xl font-bold mb-2">Post {postNumber}</h2>
       <p>This is the content of post {postNumber}.</p>
